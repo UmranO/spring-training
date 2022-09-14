e32a298c331176733d74f03dee8cd2a05f07ed02
Umran Ozman <umonal@yahoo.com>
2022-09-14 14:37:57 -0400

    Environment set up and DellCase Class created

---
+++ spring--00-Coupling/src/main/java/com/cydeo/tightly_coupled/DellCase.java
@@ -0,0 +1,13 @@
+package com.cydeo.tightly_coupled;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class DellCase {
+
+
+}
