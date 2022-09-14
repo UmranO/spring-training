066c9d6f0572b586369bcccd4f210d7695a3e8fd
Umran Ozman <umonal@yahoo.com>
2022-09-14 18:00:43 -0400

    MonitorFactory Classes created

---
+++ spring--01Core-IOC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/monitorFactory/AcerMonitor.java
@@ -0,0 +1,16 @@
+package com.cydeo.bean_annotation.monitorFactory;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AcerMonitor extends Monitor{
+    public AcerMonitor(String model, String manufacturer, int size) {
+        super(model, manufacturer, size);
+    }
+
+    public void drawPixelAt() {
+        System.out.println("Drawing pixel with Acer");
+    }
+
+}
