c43e1a504c0729e10bb12b59f5c2ae88404f61e5
Umran Ozman <umonal@yahoo.com>
2022-09-18 16:40:42 -0400

    @Qualifier implemented and tested / Mod5

--- spring--05-Core-Abstraction/src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
+++ spring--05-Core-Abstraction/src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
@@ -1,9 +1,11 @@
 package com.cydeo.proxy;
 
 import com.cydeo.model.Comment;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class EmailCommentNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
--- spring--05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ spring--05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -3,13 +3,14 @@ package com.cydeo.service;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 @Component
 public class CommentService {
-    private CommentRepository commentRepository;
-    private CommentNotificationProxy commentNotificationProxy;
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository,@Qualifier("emailCommentNotificationProxy") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
