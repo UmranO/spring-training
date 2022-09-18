9a1011198d68532b43ac60cb7881a99a757de009
Umran Ozman <umonal@yahoo.com>
2022-09-18 14:27:07 -0400

    CommentService Class / Mod5 updated

--- spring--05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ spring--05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -3,16 +3,19 @@ package com.cydeo.service;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
-
+import org.springframework.stereotype.Component;
+@Component
 public class CommentService {
-
     private CommentRepository commentRepository;
     private CommentNotificationProxy commentNotificationProxy;
-    public void publishComment (Comment comment){
-        //save in the DB  -------call the methods
-        commentRepository.storeComment(comment);
-        //send email
-        commentNotificationProxy.sendComment(comment);
 
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment (Comment comment){
+        commentRepository.storeComment(comment);  //save in the DB  -------call the methods
+        commentNotificationProxy.sendComment(comment);  //send email
     }
 }
