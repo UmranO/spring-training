f3c512ad1fce8beb0a97896546495d8234bdfab3
Umran Ozman <umonal@yahoo.com>
2022-09-18 10:34:52 -0400

    @Componenet added to Classes / Mod5 created

--- spring--05-Core-Abstraction/src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
+++ spring--05-Core-Abstraction/src/main/java/com/cydeo/proxy/EmailCommentNotificationProxy.java
@@ -1,7 +1,9 @@
 package com.cydeo.proxy;
 
 import com.cydeo.model.Comment;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmailCommentNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
@@ -9,3 +11,4 @@ public class EmailCommentNotificationProxy implements CommentNotificationProxy{
 
     }
 }
+
