e2ec02cd8d577dd111677579dfbcf3a35de81807
Umran Ozman <umonal@yahoo.com>
2022-09-18 17:14:15 -0400

    Singleton Mod6

--- spring--06-Core-BeanScope/src/main/java/com/cydeo/CommentApp.java
+++ spring--06-Core-BeanScope/src/main/java/com/cydeo/CommentApp.java
@@ -1,5 +1,28 @@
 package com.cydeo;
 
+import com.cydeo.config.CommentConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class CommentApp {
+    public static void main(String[] args) {
+
+
+        Comment comment=new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext container=new AnnotationConfigApplicationContext(CommentConfig.class);
+
+        CommentService cs1=container.getBean(CommentService.class);
+        CommentService cs2=container.getBean(CommentService.class);
+
+        System.out.println(cs1);  //same object/ same reference / same address
+        System.out.println(cs2);
+        System.out.println(cs1==cs2);
+
+    }
 
 }
