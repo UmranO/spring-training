bcac92fa49f413f080dfe8d460cca6bd8b4bcbfa
Umran Ozman <umonal@yahoo.com>
2022-09-25 14:38:07 -0400

    @Value added to AppConfigData Class -Mod7

--- spring--07-SpringBoot-Demo/src/main/java/com/cydeo/config/AppConfigData.java
+++ spring--07-SpringBoot-Demo/src/main/java/com/cydeo/config/AppConfigData.java
@@ -1,4 +1,14 @@
 package com.cydeo.config;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+
+@Data
 public class AppConfigData {
+    @Value("${username}")
+    private String username;
+    @Value("${password}")
+    private String password;
+    @Value("${url}")
+    private String url;
 }
