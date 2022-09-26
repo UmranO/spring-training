d486b236e59e8a77eb52c726dfb60c99db8730df
Umran Ozman <umonal@yahoo.com>
2022-09-25 20:09:10 -0400

    App.ConfigData field name change to myUserName-Mod7

--- spring--07-SpringBoot-Demo/src/main/java/com/cydeo/config/AppConfigData.java
+++ spring--07-SpringBoot-Demo/src/main/java/com/cydeo/config/AppConfigData.java
@@ -2,13 +2,23 @@ package com.cydeo.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
 
 @Data
+@Configuration
 public class AppConfigData {
-    @Value("${username}")
-    private String username;
+
+    @Value("${myUserName}")
+    private String myUserName;
+
     @Value("${password}")
     private String password;
+
     @Value("${url}")
     private String url;
+
+
+
 }
