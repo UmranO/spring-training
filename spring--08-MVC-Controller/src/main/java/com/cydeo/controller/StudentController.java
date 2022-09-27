7fb6fd079742f8e74a593a68647501bedbe91dbc
Umran Ozman <umonal@yahoo.com>
2022-09-27 08:29:04 -0400

    StudentController created-Mod8

---
+++ spring--08-MVC-Controller/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,19 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+
+    @RequestMapping("/student")
+    public String getStudentInfo(){
+        return "student/welcome.html";
+    }
+
+    @RequestMapping("/courses")
+    public String getCourses(){
+        return "student/courses.html";
+    }
+
+}
