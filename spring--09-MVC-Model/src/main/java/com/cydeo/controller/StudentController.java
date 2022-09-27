b8b809f470e04b314505a911c362380de465c555
Umran Ozman <umonal@yahoo.com>
2022-09-27 12:03:38 -0400

    Student Controller updated-Mod9

--- spring--09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring--09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,17 +1,44 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat.UUID;
 
 @Controller
 public class StudentController {
 
     @RequestMapping("/welcome")
-    public String homePage(Model model){
-         model.addAttribute("name","Cydeo");
-         model.addAttribute("course","MVC");
-           return "student/welcome";
+    public String homePage(Model model) {
+        model.addAttribute("name", "Cydeo");
+        model.addAttribute("course", "MVC");
 
+        String subject = "Spring Boot";
+        model.addAttribute("subject",subject);
+
+        int studentId=new Random().nextInt();
+        model.addAttribute("id",studentId);
+
+        List<Integer> numbers=new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        model.addAttribute("numbers",numbers);
+
+        LocalDate dt=LocalDate.now();
+        model.addAttribute("date",dt);
+
+        Student student=new Student(78, "Umran","Ozman");
+        model.addAttribute("student",student);
+
+        return "student/welcome";
     }
 }
