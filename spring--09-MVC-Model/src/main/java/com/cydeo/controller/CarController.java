a7732f1dd8a0003650927964438037d0bb0ade23
Umran Ozman <umonal@yahoo.com>
2022-10-09 13:57:44 -0400

    carInfo Class created-Mod9

---
+++ spring--09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -0,0 +1,14 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CarController {
+
+    public String carInfo(){
+
+        return "car/car-info";
+    }
+
+
+}
