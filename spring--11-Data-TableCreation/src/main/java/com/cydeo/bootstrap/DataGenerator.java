162db1edebc9a6f097d8b2e82e40ca606c0b6b83
Umran Ozman <umonal@yahoo.com>
2022-11-05 09:51:33 -0400

    DataGenerator is updated

--- spring--11-Data-TableCreation/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ spring--11-Data-TableCreation/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,10 +1,19 @@
 package com.cydeo.bootstrap;
 
+import com.cydeo.entity.Car;
+import com.cydeo.repository.CarRepository;
 import org.springframework.boot.CommandLineRunner;
 
 public class DataGenerator implements CommandLineRunner {
+
+
+
     @Override
     public void run(String... args) throws Exception {
-        
+        Car c1 = new Car("BMW","M3");
+        Car c2 = new Car("Honda","Civic");
+        Car c3 = new Car("Toyota","Corolla");
+
+
     }
 }
