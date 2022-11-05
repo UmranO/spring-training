243da57af3c108ce37ee99c53bfea83b0feb3be8
Umran Ozman <umonal@yahoo.com>
2022-11-05 08:45:46 -0400

    car class updated

--- spring--11-Data-TableCreation/src/main/java/com/cydeo/entity/Car.java
+++ spring--11-Data-TableCreation/src/main/java/com/cydeo/entity/Car.java
@@ -1,11 +1,16 @@
 package com.cydeo.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@Data
 public class Car {
 
     @Id
@@ -14,5 +19,8 @@ public class Car {
     private String make;
     private String model;
 
-
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
 }
