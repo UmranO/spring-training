5fb8173141baeefe2a1d4c921355fdf781f87ebf
Umran Ozman <umonal@yahoo.com>
2022-11-06 11:20:40 -0500

    Merchant class updated

--- spring--12-Data-Relationship/src/main/java/com/cydeo/entity/Merchant.java
+++ spring--12-Data-Relationship/src/main/java/com/cydeo/entity/Merchant.java
@@ -17,4 +17,21 @@ public class Merchant {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+    private String code;
+    private BigDecimal transactionFee;
+    private BigDecimal commissionRate;
+    private Integer payoutDelayCount;
+
+
+
+    public Merchant(String name, String code, BigDecimal transactionFee, BigDecimal commissionRate, Integer payoutDelayCount) {
+        this.name = name;
+        this.code = code;
+        this.transactionFee = transactionFee;
+        this.commissionRate = commissionRate;
+        this.payoutDelayCount = payoutDelayCount;
+    }
 }
+
+
