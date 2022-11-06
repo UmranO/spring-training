7a8f8a55dbc86487c2caf5cc79dbfb86fb4dad4d
Umran Ozman <umonal@yahoo.com>
2022-11-06 13:59:54 -0500

    Cart class updated

--- spring--12-Data-Relationship/src/main/java/com/cydeo/entity/Cart.java
+++ spring--12-Data-Relationship/src/main/java/com/cydeo/entity/Cart.java
@@ -16,4 +16,9 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToMany
+    @JoinTable(name = "cart_item_rel",
+            joinColumns = @JoinColumn(name = "c_id"),
+            inverseJoinColumns = @JoinColumn(name = "i_id"))
+    private List<Item> items;
 }
