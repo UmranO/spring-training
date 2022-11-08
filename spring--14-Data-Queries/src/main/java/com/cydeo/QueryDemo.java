da8686ff0b7057b2b155bd44152382163079cc8c
Umran Ozman <umonal@yahoo.com>
2022-11-08 18:28:21 -0500

    QueryDemo updated

--- spring--14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
+++ spring--14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -1,6 +1,7 @@
 package com.cydeo;
 
 import com.cydeo.repository.DepartmentRepository;
+import com.cydeo.repository.EmployeeRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,12 @@ public class QueryDemo implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
     private final DepartmentRepository departmentRepository;
+    private final EmployeeRepository employeeRepository;
 
-    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
         this.regionRepository = regionRepository;
         this.departmentRepository = departmentRepository;
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
@@ -42,7 +45,8 @@ public class QueryDemo implements CommandLineRunner {
         System.out.println("findDistinctTop2ByDivisionContains:" + departmentRepository.findDistinctTop2ByDivisionContains("Hea"));
         System.out.println("---------------------------------------------------------------");
 
-//
+
+
     }
 
     }
