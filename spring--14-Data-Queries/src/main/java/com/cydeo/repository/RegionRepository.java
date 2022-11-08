aaea32ec4857febaeab023d3135f8558a2a55146
Umran Ozman <umonal@yahoo.com>
2022-11-08 13:17:32 -0500

    RegionRepository updated

--- spring--14-Data-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
+++ spring--14-Data-Queries/src/main/java/com/cydeo/repository/RegionRepository.java
@@ -1,6 +1,5 @@
 package com.cydeo.repository;
 
-import com.cydeo.entity.Course;
 import com.cydeo.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +7,28 @@ import java.util.List;
 
 public interface RegionRepository extends JpaRepository<Region,Integer> {
 
+    //Display all regions in Canada
+    List<Region> findByCountry(String Country);
+
+
+    //Display all regions with country name includes 'United'
+    List<Region> findByCountryContains(String CountryName);
+
+
+    //Display all regions with country name includes 'United' in order(region)
+    List<Region> findByCountryContainsOrderByRegion(String country);
+
+
+    //Display top 2 regions in United States
+    List<Region>findTopByCountry(String Country);
+    List<Region>findTop2ByCountry(String Country);
+
+
+    //Display all regions with country name includes 'United' in order(region)
+    List<Region>findTop3ByCountry(String Country);
+
+
+    List<Region> findTopByCountryContainsOrderByRegion(String country);
 
 
 
