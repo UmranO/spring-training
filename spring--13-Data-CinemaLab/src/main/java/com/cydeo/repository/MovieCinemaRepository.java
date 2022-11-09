9f6bbd1a3b86c4777192b717e8aa19e51e815a2a
Umran Ozman <umonal@yahoo.com>
2022-11-09 16:02:05 -0500

    MovieCinemaRepository renamed

--- spring--13-Data-CinemaLab/src/main/java/com/cydeo/repository/MovieCinemaRepository.java
+++ spring--13-Data-CinemaLab/src/main/java/com/cydeo/repository/MovieCinemaRepository.java
@@ -2,7 +2,7 @@ package com.cydeo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovieCinema extends JpaRepository<MovieCinema,Long> {
+public interface MovieCinemaRepository extends JpaRepository<MovieCinemaRepository,Long> {
 
 
 // ------------------- DERIVED QUERIES ------------------- //
