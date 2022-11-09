6a96388355f2d659e7e6b3a14ef1d0cb82443665
Umran Ozman <umonal@yahoo.com>
2022-11-09 11:34:11 -0500

    EmployeeRepository updated

--- spring--14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring--14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -3,25 +3,22 @@ package com.cydeo.repository;
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
-
-public interface EmployeeRepository extends JpaRepository <Employee,Integer> {
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
     //Display all employees with email address ""
     List<Employee> findByEmail(String email);
 
     //Display all employees with firstname "" and last name "",
-    List<Employee> findByFirstNameAndLastName(String firstName, String lastName);
     //also show all employees with an email address ""
-    List<Employee> findByFirstNameAndLastNameOrEmail(String firstName, String lastName,String email);
-
+    List<Employee> findByFirstNameAndLastNameOrEmail(String firstname,String lastname,String email);
 
     //Display all employees that first name is not ""
-    List <Employee> findByFirstNameIsNot (String firstname);
+    List<Employee> findByFirstNameIsNot(String firstname);
 
     //Display all employees where last name starts with ""
     List<Employee> findByLastNameStartsWith(String pattern);
@@ -33,8 +30,7 @@ public interface EmployeeRepository extends JpaRepository <Employee,Integer> {
     List<Employee> findBySalaryLessThan(Integer salary);
 
     //Display all employees that has been hired between "" and ""
-    List<Employee> findByHireDateBetween(LocalDate startDate, LocalDate EndDate);
-
+    List<Employee> findByHireDateBetween(LocalDate startDate,LocalDate endDate);
 
     //Display all employees where salaries greater and equal to "" in order-salary
     List<Employee> findBySalaryGreaterThanEqualOrderBySalary(Integer salary);
@@ -42,13 +38,61 @@ public interface EmployeeRepository extends JpaRepository <Employee,Integer> {
     //Display top unique 3 employees that is making less than ""
     List<Employee> findDistinctTop3BySalaryLessThan(Integer salary);
 
-
     //Display all employees that do not have email address
     List<Employee> findByEmailIsNull();
+
 
     @Query("SELECT employee FROM Employee employee WHERE employee.email='amcnee1@google.es'")
     Employee retrieveEmployeeDetail();
 
+
     @Query("SELECT e.salary FROM Employee e WHERE e.email='amcnee1@google.es'")
     Integer retrieveEmployeeSalary();
+
+    //Not Equal
+    @Query("SELECT e FROM Employee e WHERE e.salary <> ?1")
+    List<Employee> retrieveEmployeeSalaryNotEqual(int salary);
+
+    //Like / Contains / Starts-with / Ends-with
+    @Query("SELECT e FROM Employee e WHERE e.firstName LIKE ?1")
+    List<Employee> retrieveEmployeeFirstNameLike(String pattern);
+
+    //Less Than
+    @Query("SELECT e FROM Employee e WHERE e.salary < ?1")
+    List<Employee> retrieveEmployeeSalaryLessThan(int salary);
+
+    //Greater Than
+    @Query("SELECT e.firstName FROM Employee e WHERE e.salary > ?1")
+    List<String> retrieveEmployeeSalaryGreaterThan(int salary);
+
+    //BETWEEN
+    @Query("SELECT e FROM Employee e WHERE e.salary BETWEEN ?1 AND ?2")
+    List<Employee> retrieveEmployeeSalaryBetween(int salary1,int salary2);
+
+    //BEFORE
+    @Query("SELECT e FROM Employee e WHERE e.hireDate > ?1")
+    List<Employee> retrieveEmployeeHireDateBefore(LocalDate date);
+
+    //NULL
+    @Query("SELECT e FROM Employee e WHERE e.email IS NULL")
+    List<Employee> retrieveEmployeeEmailIsNull();
+
+    //NOT NULL
+    @Query("SELECT e FROM Employee e WHERE e.email IS NOT NULL")
+    List<Employee> retrieveEmployeeEmailIsNotNull();
+
+    //SORTING in Asc Order
+    @Query("SELECT e FROM Employee e ORDER BY e.salary")
+    List<Employee> retrieveEmployeeSalaryOrderAsc();
+
+    //SORTING in Desc Order
+    @Query("SELECT e FROM Employee e ORDER BY e.salary DESC")
+    List<Employee> retrieveEmployeeSalaryOrderDesc();
+
+
+
+
+
+
+
 }
