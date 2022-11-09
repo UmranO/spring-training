a3a4d460524e994fc0d9a0242e0e202a1fc18522
Umran Ozman <umonal@yahoo.com>
2022-11-08 20:19:15 -0500

    QueryDemo updated

--- spring--14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
+++ spring--14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -47,6 +47,7 @@ public class QueryDemo implements CommandLineRunner {
 
         System.out.println("-----------EMPLOYEES----------");
         System.out.println(employeeRepository.retrieveEmployeeDetail());
+        System.out.println(employeeRepository.retrieveEmployeeSalary());
 
     }
 
