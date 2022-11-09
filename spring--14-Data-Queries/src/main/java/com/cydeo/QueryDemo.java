b2af4c4abccdfd6fea8c1b79a0d61230f894212d
Umran Ozman <umonal@yahoo.com>
2022-11-08 23:13:04 -0500

    QueryDemo updated

--- spring--14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
+++ spring--14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import com.cydeo.repository.CourseRepository;
 import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.EmployeeRepository;
 import com.cydeo.repository.RegionRepository;
@@ -12,11 +13,13 @@ public class QueryDemo implements CommandLineRunner {
     private final RegionRepository regionRepository;
     private final DepartmentRepository departmentRepository;
     private final EmployeeRepository employeeRepository;
+    private final CourseRepository courseRepository;
 
-    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository, CourseRepository courseRepository) {
         this.regionRepository = regionRepository;
         this.departmentRepository = departmentRepository;
         this.employeeRepository = employeeRepository;
+        this.courseRepository = courseRepository;
     }
 
     @Override
@@ -47,8 +50,19 @@ public class QueryDemo implements CommandLineRunner {
 
         System.out.println("-----------EMPLOYEES----------");
         System.out.println(employeeRepository.retrieveEmployeeDetail());
+        System.out.println("---------------------------------------------------------------");
         System.out.println(employeeRepository.retrieveEmployeeSalary());
 
+        System.out.println("-----------COURSES------------");
+        System.out.println(courseRepository.findByCategoryOrderByName("Spring"));
+        System.out.println("---------------------------------------------------------------");
+        System.out.println(courseRepository.existsCourseByName("Rapid Spring Boot Application Development"));
+        System.out.println("---------------------------------------------------------------");
+        System.out.println(courseRepository. countByCategory("Spring"));
+        System.out.println("---------------------------------------------------------------");
+        System.out.println(courseRepository.findByNameStartingWith("Rapid"));
+        System.out.println("---------------------------------------------------------------");
+        System.out.println(courseRepository.findByRating(5));
     }
 
     }
