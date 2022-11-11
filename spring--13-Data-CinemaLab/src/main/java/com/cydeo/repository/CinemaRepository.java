d9d323ec4eb1a0dedc00a87ee2d1e0174e8e5a18
Umran Ozman <umonal@yahoo.com>
2022-11-10 20:10:53 -0500

    CinemaRepository updated

--- spring--13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ spring--13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -26,5 +26,31 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
     //Write a derived query to list all cinemas with a specific name or sponsored name
     List<Cinema> findAllByNameOrSponsoredName(String name, String sponsoredName);
 
+    // ------------------- JPQL QUERIES ------------------- //
+
+    //Write a JPQL query to read the cinema name with a specific id
+    @Query("SELECT c.name FROM Cinema c WHERE c.id = ?1")
+    String fetchById(@Param("id") Long id);
+
+    // ------------------- Native QUERIES ------------------- //
+
+    //Write a native query to read all cinemas by location country
+    @Query(value = "SELECT * FROM cinema c JOIN location l " +
+            "ON l.id = c.location_id WHERE l.country = ?1", nativeQuery = true)
+    List<Cinema> retrieveAllBasedOnLocationCountry(@Param("locationCountry") String locationCountry);
+
+    //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
+    @Query(value = "SELECT * FROM cinema WHERE name ILIKE concat('%', ?1, '%') " +
+            "OR sponsored_name ILIKE concat('%', ?1, '%')", nativeQuery = true)
+    List<Cinema> retrieveAllByNameOrSponsoredName(@Param("pattern") String pattern);
+
+    //Write a native query to sort all cinemas by name
+    @Query(value = "SELECT * FROM cinema ORDER BY name", nativeQuery = true)
+    List<Cinema> sortByName();
+
+    //Write a native query to distinct all cinemas by sponsored name
+    @Query(value = "SELECT DISTINCT sponsored_name FROM cinema", nativeQuery = true)
+    List<String> distinctBySponsoredName();
+
 
 }
