149a27d1e812e6d9850f0967884c32228e8d5711
Umran Ozman <umonal@yahoo.com>
2022-11-28 17:31:23 -0500

    CourseController_ResponseWrapper  Class created

---
+++ spring--15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
@@ -0,0 +1,17 @@
+package com.cydeo.controller;
+
+import com.cydeo.service.CourseService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses/api/v3")
+public class CourseController_ResponseWrapper {
+
+    private final CourseService courseService;
+
+    public CourseController_ResponseWrapper(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+}
