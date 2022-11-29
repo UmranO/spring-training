867d6b5e9a0f1983bd0dc1a694d9ff93eb43336e
Umran Ozman <umonal@yahoo.com>
2022-11-28 20:03:03 -0500

    CourseController_ResponseEntity  Class updated

--- spring--15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
+++ spring--15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -43,6 +43,10 @@ public class CourseController_ResponseEntity {
                 .status(HttpStatus.CREATED)
                 .header("Operation", "Create")
                 .body(courseService.createCourse(course));
-
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteCourseById(@PathVariable("id") Long courseId){
+        courseService.deleteCourseById(courseId);
+        return ResponseEntity.noContent().build();
     }
 }
