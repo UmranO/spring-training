392495b912454841377fd282a180469c4fc206e1
Umran Ozman <umonal@yahoo.com>
2022-12-03 20:54:18 -0500

    module 19 application.OpenAPI Bean added to runner class

--- spring--19-REST-OpenAPI3/src/main/java/com/cydeo/Spring19RestOpenApi3Application.java
+++ spring--19-REST-OpenAPI3/src/main/java/com/cydeo/Spring19RestOpenApi3Application.java
@@ -21,7 +21,14 @@ public class Spring19RestOpenApi3Application {
     public ModelMapper mapper() {
         return new ModelMapper();
     }
-
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(new Info()
+                        .title("Cydeo Application OpenAPI")
+                        .version("v1")
+                        .description("Cydeo application API documentation"))
+                .servers(List.of(new Server().url("https://dev.cydeo.com").description("Dev Environment")));
+    }
 
 
 }
