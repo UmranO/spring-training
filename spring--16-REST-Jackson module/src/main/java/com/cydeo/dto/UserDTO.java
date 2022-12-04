5e109583a470923fe91aab65e1c804dd79a86574
Umran Ozman <umonal@yahoo.com>
2022-12-04 14:38:24 -0500

    module 16 UserDTO class updated @Json..

--- spring--16-REST-Jackson module/src/main/java/com/cydeo/dto/UserDTO.java
+++ spring--16-REST-Jackson module/src/main/java/com/cydeo/dto/UserDTO.java
@@ -2,7 +2,9 @@ package com.cydeo.dto;
 
 
 import com.cydeo.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +12,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
 
     private String email;
 
-
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String username;
     private UserRole role;
