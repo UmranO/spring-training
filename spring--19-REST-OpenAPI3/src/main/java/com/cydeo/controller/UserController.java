b237337c627240fe4aca5d85f53c6aa0292940b7
Umran Ozman <umonal@yahoo.com>
2022-12-03 21:20:13 -0500

    module 19 UserController updated / @Operation added

--- spring--19-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
+++ spring--19-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +22,14 @@ public class UserController {
     }
 
     @GetMapping("/list")
+    @Operation(summary = "Read all users")
     public ResponseEntity<List<UserDTO>> getAllUsers() {
+
         return ResponseEntity.ok(userService.getUsers());
     }
 
     @PostMapping
+    @Operation(summary = "Create a user")
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(userDTO));
     }
