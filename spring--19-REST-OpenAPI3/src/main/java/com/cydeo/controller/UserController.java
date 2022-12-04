b505c7707dc55b2f030728591313a7cb3b3ec994
Umran Ozman <umonal@yahoo.com>
2022-12-03 21:47:26 -0500

    module 19 UserController updated / @ApiResponse / @Content added

--- spring--19-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
+++ spring--19-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
@@ -3,6 +3,9 @@ package com.cydeo.controller;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +26,12 @@ public class UserController {
 
     @GetMapping("/list")
     @Operation(summary = "Read all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved users (OK)",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+    })
     public ResponseEntity<List<UserDTO>> getAllUsers() {
 
         return ResponseEntity.ok(userService.getUsers());
