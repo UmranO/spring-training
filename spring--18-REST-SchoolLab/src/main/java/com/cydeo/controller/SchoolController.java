ba81b4d51ee0216829b89a74848103d85e5bba58
Umran Ozman <umonal@yahoo.com>
2022-12-05 08:42:36 -0500

    module 18 SchoolController updated

--- spring--18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
+++ spring--18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
@@ -1,10 +1,11 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ResponseWrapper;
-import com.cydeo.dto.StudentDTO;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.ParentService;
 import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +17,12 @@ public class SchoolController {
 
     private final TeacherService teacherService;
     private final StudentService studentService;
+    private final ParentService parentService;
 
-    public SchoolController(TeacherService teacherService, StudentService studentServiceService, StudentService studentService) {
+    public SchoolController(TeacherService teacherService, StudentService studentServiceService, StudentService studentService, ParentService parentService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
+        this.parentService = parentService;
     }
 
     @GetMapping("/teachers")
@@ -33,6 +36,12 @@ public class SchoolController {
     public ResponseEntity<ResponseWrapper> readAllStudents() {
         return ResponseEntity.ok(new ResponseWrapper("Students are successfully retrieved", studentService.findAll()));
     }
-
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> readAllParents() {
+        ResponseWrapper responseWrapper =
+                new ResponseWrapper(true, "Parents are retrieved successfully",
+                        HttpStatus.OK.value(), parentService.findAll());
+        return ResponseEntity.status(HttpStatus.OK).body(responseWrapper);
+    }
 
 }
