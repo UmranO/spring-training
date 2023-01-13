61ec6340ffa181bf43f3b5fa3874badc8ae7e3c2
Umran Ozman <umonal@yahoo.com>
2023-01-12 21:32:00 -0500

    module 21 - LoggingAspect updated

--- spring--21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring--21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.cydeo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,14 +14,27 @@ public class LoggingAspect {
 
     Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
-    public void myPointcut() {}
+//    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
+//    public void myPointcut() {}
+//
+//    @Before("myPointcut()")
+//    public void log() {
+//        logger.info("Info log..........");
+//    }
 
-    @Before("myPointcut()")
-    public void log() {
-        logger.info("Info log..........");
+//    @Before("execution(* com.cydeo.controller.CourseController.*(..))")
+//    public void log() {
+//        logger.info("Info log..........");
+//    }
+
+    @Pointcut("execution(* com.cydeo.repository.CourseRepository.findById(*))")
+    public void courseRepositoryFindByIdPC() {}
+
+    @Before("courseRepositoryFindByIdPC()")
+    public void beforeCourseRepositoryFindById(JoinPoint joinPoint) {
+        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
     }
-
 
 
 }
