e30af8ca23078b5bfcac63a03c10c90fb2498d7d
Umran Ozman <umonal@yahoo.com>
2023-02-22 10:50:04 -0500

    module 22 - CalculatorTest Class created

---
+++ spring--22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        System.out.println("Add method");
+        int actual = Calculator.add(2,3);
+        assertEquals(5,actual);                     //JUnit5 expected value is written 1st & actual is the 2nd
+        assertEquals(5,actual,"Test failed");       //we can also add a fail message
+    }
+    @Test
+    void testCase1(){
+      //  System.out.println("Test Case 1");
+        fail("Not implemented yet.");
+    }
+    @Test
+    void testCase2(){
+        System.out.println("Test Case 2");
+        assertEquals("add", Calculator.operator);   //It is same as assertTrue
+        assertTrue(Calculator.operator.equals("add"));       //It is same as assertEquals
+    }
+    @Test
+    void testCase3(){
+        System.out.println("Test Case 3");
+    }
+    @Test
+    void testCase4(){
+        System.out.println("Test Case 4");
+    }
+    @Test
+    void testCase5(){
+        System.out.println("Test Case 5");
+    }
+
+}
